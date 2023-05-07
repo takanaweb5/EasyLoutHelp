8e96dd2a569d209a1a358a00b20332eb86a760a2
takanaweb5 <50874513+takanaweb5@users.noreply.github.com>
2023-05-07 21:20:05 +0900

    htmlファイル更新

---
+++ docs/GetLog.js
@@ -0,0 +1,35 @@
+﻿// XMLHttpRequestオブジェクト作成
+var xmlhttp = createXmlHttp();
+
+var url = "php/AccessLog.php";
+var post = "href="      + encodeURIComponent(location.href)
+         + "&referer="  + encodeURIComponent(document.referrer)
+         + "&userAgent="+ encodeURIComponent(navigator.userAgent);
+var url = url + "?" + post;
+
+if (xmlhttp != null) {
+  xmlhttp.open('GET', url, true);
+  xmlhttp.send();
+}
+
+function createXmlHttp(){
+  if(window.ActiveXObject){
+    //Windows IE用
+    try {
+      return new ActiveXObject('Msxml2.XMLHTTP');
+    } catch (e) {
+      try {
+        return new ActiveXObject('Microsoft.XMLHTTP');
+      } catch (e) {
+        return null;
+      }
+    }
+  }
+  else if(window.XMLHttpRequest){
+    //Windows IE以外のXMLHttpRequestオブジェクト実装ブラウザ用
+    return new XMLHttpRequest();
+  }
+  else {
+    return null;
+  }
+}
