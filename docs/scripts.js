20051ce5ac43838240eac5e739de2b97975d49cc
takanaweb5 <50874513+takanaweb5@users.noreply.github.com>
2023-08-20 16:30:34 +0900

    コード整備

--- docs/scripts.js
+++ docs/scripts.js
@@ -1,49 +1,21 @@
 function openMenu(event) {
- console.log("openMenu");
- // setLinkClick();
- var tocContainer = document.getElementById("menu");
- var closeButton = document.getElementById("menu-button");
- tocContainer.style.left = "0";
- closeButton.innerHTML = "　";
+ var menu = document.getElementById("menu");
+ var menuButton = document.getElementById("menu-button");
+ menu.style.left = "0";
+ menuButton.innerHTML = "　";
  event.stopPropagation(); // クリックイベントの伝播を停止
 }
 
 function closeMenu(event) {
- console.log("closeMenu");
- var tocContainer = document.getElementById("menu");
- var closeButton = document.getElementById("menu-button");
- if (closeButton.innerHTML != "　") { return; }
- // removeLinkClick();
- tocContainer.style.left = "-220px";
- closeButton.innerHTML = "&#9776;";
-
+ var menu = document.getElementById("menu");
+ var menuButton = document.getElementById("menu-button");
+ if (menuButton.innerHTML != "　") { return; }
+ menu.style.left = "-220px";
+ menuButton.innerHTML = "☰";
 }
 
-// function loadToc() {
-//  var tocContainer = document.getElementById("menu");
-//  var xhr = new XMLHttpRequest();
-//  xhr.open("GET", "LeftPage.htm", true);
-//  xhr.onreadystatechange = function () {
-//   if (xhr.readyState === 4 && xhr.status === 200) {
-//    tocContainer.innerHTML = xhr.responseText;
-//   }
-//  };
-//  xhr.send();
-// }
-
-function loadSection(url, id) {
- var element = document.getElementById(id);
- var xhr = new XMLHttpRequest();
- xhr.open("GET", url, true);
- xhr.onreadystatechange = function () {
-  if (xhr.readyState === 4 && xhr.status === 200) {
-   element.innerHTML = xhr.responseText;
-  }
- };
- xhr.send();
-}
-
-function loadMenu(url) {
+function makeMenu() {
+ const url = "LeftPage.htm";
  var xhr = new XMLHttpRequest();
  xhr.open("GET", url, true);
  xhr.onreadystatechange = function () {
@@ -52,59 +24,27 @@ function loadMenu(url) {
    var parser = new DOMParser();
    var externalDoc = parser.parseFromString(xhr.responseText, "text/html");
    // 指定したタグの前に外部HTMLの内容を挿入
-   var element = document.getElementsByClassName("WordSection1")[0];
-   element.parentNode.insertBefore(externalDoc.getElementById("menu"), element);
+   var main = document.getElementsByClassName("WordSection1")[0];
+   main.parentNode.insertBefore(externalDoc.getElementById("menu"), main);
   }
  };
  xhr.send();
 }
 
-function linkClick(event) {
- event.preventDefault(); // リンクのデフォルト動作をキャンセル
- console.log("linkClick");
- removeLinkClick();
- closeMenu(event);
- var url = this.getAttribute("href");
- loadSection(url, "content");
-}
-
-function setLinkClick() {
- console.log("setLinkClick");
- // すべての<a>要素に対して処理を追加
- var linkElements = document.getElementById("toc-container").querySelectorAll("a");
- linkElements.forEach(function (linkElement) {
-  linkElement.addEventListener("click", linkClick);
- });
-}
-
-function removeLinkClick() {
- console.log("removeLinkClick");
- // すべての<a>要素に対して処理を追加
- var linkElements = document.getElementById("toc-container").querySelectorAll("a");
- linkElements.forEach(function (linkElement) {
-  linkElement.removeEventListener("click", linkClick);
- });
-}
-
-// ページ読み込み時にsection1.htmlをデフォルトで表示
+// ページ読み込み時
 window.onload = function () {
- loadMenu('LeftPage.htm');
- createMenuButton();
- // loadSection('Introduction.htm', "content");
+ makeMenu();
+ const main = document.getElementsByClassName("WordSection1")[0];
+ createMenuButton(main);
+ main.addEventListener("click", closeMenu);
 }
 
-
-function createMenuButton() {
- var newDiv = document.createElement("div");
+function createMenuButton(main) {
+ const newDiv = document.createElement("div");
  newDiv.textContent = "☰";
  newDiv.id = "menu-button";  // IDを付与
-
- // クリックイベントを割り当てる
  newDiv.addEventListener("click", openMenu);
-
- // 特定のIDを持つ要素の後ろに新しいdiv要素を挿入
- var element = document.getElementsByClassName("WordSection1")[0];
- element.insertAdjacentElement("afterbegin", newDiv);
+ main.insertAdjacentElement("afterbegin", newDiv);
 }
 
 
